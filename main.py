c3ad98ea1fb415ac6c7f8e5c952d8b89e2d8f5f7
wangting <14209892+yachenyanyi@user.noreply.gitee.com>
2025-07-01 15:42:06 +0800

    修正了几个错别字

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 YachenYanyi
+# Copyright (C) 2025 yachenyanyi
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
